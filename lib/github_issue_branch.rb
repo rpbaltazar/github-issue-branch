9740413573cf2f9bd08cfa65acb3d5c6c56de00a
Rui Baltazar <rui.p.baltazar@gmail.com>
2016-01-31 11:49:12 +0800

    listing open issues

--- lib/github_issue_branch.rb
+++ lib/github_issue_branch.rb
@@ -11,16 +11,18 @@ class GithubIssueBranch
     end
   end
 
-  GITHUB_TOKEN_FILE = ['.github_issue_branch_auth', "#{ENV['HOME']}/.github_issue_branch_auth"]
+  GITHUB_CONF_FILE = ['.github_issue_branch_auth', "#{ENV['HOME']}/.github_issue_branch_auth"]
 
   def initialize
-    @github_conf = YAML.load_file github_issue_branch_auth
-    @github = Github.new(oauth_token: load_token)
+    @github_conf = YAML.load_file github_auth_file
+    @github = Github.new(oauth_token: @github_conf)
   end
 
   def list_issues
-    #@github
-    # fetch open issues from github and print the name
+    # TODO: read this from either config file or from git remotes
+    list = @github.issues.list user: 'intelllex', repo: 'integrated'
+    @issue_list = list.select { |issue| issue['pull_request'].nil? }
+    print_list
   end
 
   def choose_issue issue_id
@@ -28,10 +30,14 @@ class GithubIssueBranch
     # create branch locally from current branch with name normalization-issue_id
   end
 
+  def print_list
+    @issue_list.each { |issue| puts issue.title }
+  end
+
   private
 
   def github_auth_file
-    conf_file = GITHUB_AUTH_FILE.select { |conf_file| File.exists? conf }.first
+    conf_file = GITHUB_CONF_FILE.select { |conf_file| File.exists? conf_file }.first
     raise Error::AuthFileNotFound unless conf_file
     conf_file
   end
